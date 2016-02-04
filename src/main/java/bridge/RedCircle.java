971efd0b6e6b9c8d36a94436383a00dfc054d4dd
Jary Du <ydu@apple.com>
2016-02-04 00:01:59 -0800

    add bridge pattern

---
+++ src/main/java/bridge/RedCircle.java
@@ -0,0 +1,10 @@
+package bridge;
+
+/**
+ * Created by jarydu on 2/3/16.
+ */
+public class RedCircle implements DrawAPI {
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Draw a red circle with radius[" + radius + "] at [" + x + ":" + y + "]");
+    }
+}
