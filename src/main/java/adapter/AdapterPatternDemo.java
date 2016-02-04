0219defbe8071a40fa2c501c18c7ef155b31b70b
Jary Du <ydu@apple.com>
2016-02-03 23:30:49 -0800

    add adapter pattern example

---
+++ src/main/java/adapter/AdapterPatternDemo.java
@@ -0,0 +1,14 @@
+package adapter;
+
+/**
+ * Created by jarydu on 2/3/16.
+ */
+public class AdapterPatternDemo {
+
+    public static void main(String[] args) {
+        AudioPlayer player = new AudioPlayer();
+
+        player.play("mp4", "beyond the horizon!");
+        player.play("vlc", "far far away");
+    }
+}
