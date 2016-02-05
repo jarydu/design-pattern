454096748a9cb6003888bc38b17f7b4fe7b58a15
Jary Du <ydu@apple.com>
2016-02-04 23:18:19 -0800

    add facade pattern

---
+++ src/main/java/facade/Circle.java
@@ -0,0 +1,10 @@
+package facade;
+
+/**
+ * Created by jarydu on 2/4/16.
+ */
+public class Circle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
---
+++ src/main/java/facade/Shape.java
@@ -0,0 +1,8 @@
+package facade;
+
+/**
+ * Created by jarydu on 2/4/16.
+ */
+public interface Shape {
+    void draw();
+}
