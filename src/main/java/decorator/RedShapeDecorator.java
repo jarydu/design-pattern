3617e0fa3066781d3ab8a1b08a9f065d155183dc
jary.du@gmail.com <jary.du@gmail.com>
2016-02-04 23:50:18 -0800

    add decorator pattern

---
+++ src/main/java/decorator/RedShapeDecorator.java
@@ -0,0 +1,17 @@
+package decorator;
+
+/**
+ * Created by jarydu on 2/4/16.
+ */
+public class RedShapeDecorator extends ShapeDecorator {
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        System.out.println("adding the extra red color feature");
+    }
+}
---
+++ src/main/java/decorator/Shape.java
@@ -0,0 +1,8 @@
+package decorator;
+
+/**
+ * Created by jarydu on 2/4/16.
+ */
+public interface Shape {
+    public void draw();
+}
